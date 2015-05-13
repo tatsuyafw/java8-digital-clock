6a30086ee149f9c37c4b31eee6f31ca6a105cf99
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-05-14 00:35:18 +0900

    Filtering data with query

--- src/main/java/common/Clock.java
+++ src/main/java/common/Clock.java
@@ -9,12 +9,18 @@ import java.util.ResourceBundle;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -24,15 +30,35 @@ public class Clock extends Application implements Initializable {
     private static final String LAYOUT_FILE = "layout.fxml";
     private static final String STYLESHEET_FILE = "style.css";
 
-    private DateTimeFormatter formatter;
+    @FXML private TextField query;
     @FXML private Label clockLabel;
+    @FXML private ListView<String> searchResult;
+
+    private DateTimeFormatter formatter;
+    private ObservableList<String> masterData = FXCollections.observableArrayList("item1", "item2", "item3");
+    private FilteredList<String> filteredResult;
 
     public Clock() {
         formatter = DateTimeFormatter.ofPattern("HH:mm ss");
     }
 
+    private ChangeListener<String> onQueryChanged() {
+        /**
+         * See: http://code.makery.ch/blog/javafx-2-event-handlers-and-change-listeners/
+         */
+        return (property, oldValue, newValue) -> {
+            filteredResult.setPredicate(item -> {
+                return item.contains(newValue);
+            });
+        };
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        filteredResult = new FilteredList<>(masterData, p -> true);
+        query.textProperty().addListener(onQueryChanged());
+
+        searchResult.setItems(filteredResult);
         clockLabel.setText(formattedDate());
         Timeline timer = new Timeline(new KeyFrame(Duration.millis(CLOCK_RENDERING_DELAY), event -> {
             clockLabel.setText(formattedDate());
@@ -55,7 +81,6 @@ public class Clock extends Application implements Initializable {
             System.exit(1);
         }
 
-        //Scene scene = new Scene(root, 600, 800);
         Scene scene = new Scene(root);
         String style = getClass().getResource(STYLESHEET_FILE).toExternalForm();
         scene.getStylesheets().add(style);
