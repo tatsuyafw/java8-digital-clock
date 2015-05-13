b32dc12725e9fc4771f561b257eea0a15dbc5ca3
Tatsuya Hoshino <tatsuya7.hoshino7@gmail.com>
2015-05-14 01:04:28 +0900

    Set the clock transparent when double clicked

--- src/main/java/common/Clock.java
+++ src/main/java/common/Clock.java
@@ -13,6 +13,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -21,6 +22,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -29,13 +32,15 @@ public class Clock extends Application implements Initializable {
     private static final String CLOCK_TITLE = "Digital clock";
     private static final String LAYOUT_FILE = "layout.fxml";
     private static final String STYLESHEET_FILE = "style.css";
+    private static final String[] DUMMY_DATA = {
+        "Java", "JavaScript", "ShellScript", "Ruby", "Python", "Perl", "Go", "PHP", "Haskell", "Clojure", "C", "C++", "Lua" };
 
     @FXML private TextField query;
     @FXML private Label clockLabel;
     @FXML private ListView<String> searchResult;
 
     private DateTimeFormatter formatter;
-    private ObservableList<String> masterData = FXCollections.observableArrayList("item1", "item2", "item3");
+    private ObservableList<String> masterData = FXCollections.observableArrayList(DUMMY_DATA);
     private FilteredList<String> filteredResult;
 
     public Clock() {
@@ -48,24 +53,45 @@ public class Clock extends Application implements Initializable {
          */
         return (property, oldValue, newValue) -> {
             filteredResult.setPredicate(item -> {
-                return item.contains(newValue);
+                // TODO: 大文字小文字を区別するかどうかを設定できるようにする
+                return item.toLowerCase().contains(newValue.toLowerCase());
             });
+        };
+    }
+
+    private EventHandler<? super MouseEvent> onClockClicked() {
+        return event -> {
+            if (event.getButton().equals(MouseButton.PRIMARY)) {
+                if (event.getClickCount() == 2) {
+                    System.out.println("Double clicked");
+                    String clockColor = clockLabel.getStyle().replace("-fx-opacity: ", "");
+                    if (clockColor.equals("") || clockColor.equals("0.0")) {
+                        clockLabel.setStyle("-fx-opacity: 1.0");
+                    } else {
+                        clockLabel.setStyle("-fx-opacity: 0.0");
+                    }
+                    System.out.println(clockLabel.getStyle());
+                }
+            }
         };
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        filteredResult = new FilteredList<>(masterData, p -> true);
-        query.textProperty().addListener(onQueryChanged());
-
-        searchResult.setItems(filteredResult);
+        // 時計の設定
         clockLabel.setText(formattedDate());
         Timeline timer = new Timeline(new KeyFrame(Duration.millis(CLOCK_RENDERING_DELAY), event -> {
             clockLabel.setText(formattedDate());
         }));
-
         timer.setCycleCount(Timeline.INDEFINITE);
         timer.play();
+
+        clockLabel.setOnMouseClicked(onClockClicked());
+
+        // 検索
+        filteredResult = new FilteredList<>(masterData, p -> true);
+        query.textProperty().addListener(onQueryChanged());
+        searchResult.setItems(filteredResult);
     };
 
     @Override
